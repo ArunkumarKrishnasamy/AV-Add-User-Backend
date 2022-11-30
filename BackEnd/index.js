9ee4a06771541f01aa05c08114adc54881502df6
ARUNKUMAR <97109931+ArunkumarKrishnasamy@users.noreply.github.com>
2022-11-30 22:18:37 +0100

    uploaded

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,6 +13,7 @@ app.use(
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
 const URL = "mongodb://localhost:27017";
+
 app.post("/users", async (req, res) => {
   try {
     // open the connection
@@ -52,20 +53,55 @@ app.get("/users/:id", async (req, res) => {
   try {
     let connection = await mongoClient.connect(URL);
     let db = connection.db("Users");
-    let users = await db.collection("Registered Users").find().toArray();
-    console.log(req.params.id);
+    let user = await db
+      .collection("Registered Users")
+      .findOne({ _id: mongodb.ObjectId(req.params.id) });
 
-    // let user = await db
-    //   .collection("Registered Users")
-    //   .findOne({ _id: req.params.id });
-    let userIndex = users.findIndex((obj) => obj._id == req.params.id);
-    // console.log(users[userIndex]);
     await connection.close();
-    res.json(users[userIndex]);
+    res.json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json({
       message: "Error in Finding user",
+    });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = connection.db("Users");
+    await db
+      .collection("Registered Users")
+      .updateOne({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body });
+
+    await connection.close();
+    res.json({ message: "User info Updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error in Editing user",
+    });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = connection.db("Users");
+    await db
+      .collection("Registered Users")
+      .deleteOne({ _id: mongodb.ObjectId(req.params.id) });
+
+    // let userIndex = users.findIndex((obj) => obj._id == req.params.id);
+    // let id = users[userIndex]._id;
+    // users.splice(userIndex, 1);
+    await connection.close();
+    res.json({ message: "User Deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error in Deleting user",
     });
   }
 });
